6b814081726d6c3fe68bafc2afaa9f90ee54cf82
SyedMohammed2005 <syedmdpashaquadri2005@gmail.com>
2026-05-06 08:13:50 +0530

    Initial commit with folders

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./chai";
+
+function App() {
+  return (
+    <div className="App">
+      <Chai />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ customReact/customreact.js
@@ -0,0 +1,10 @@
+ const mainContainer=document.querySelector("#root")
+
+ const reactElement=React.createElement(
+    'a',
+    {href:"https://sensai-pv4v.vercel.app/",traget:"_blank"},
+    "Visit Sensai"
+
+ )
+    ReactDOM.render(reactElement,mainContainer) 
+    
