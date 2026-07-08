6c445da734b24ea19327d8187742250bdf576d81
SyedMohammed2005 <syedmdpashaquadri2005@gmail.com>
2026-07-08 20:18:48 +0530

    Initial commit

---
+++ useRef/src/App.jsx
@@ -0,0 +1,28 @@
+import {useRef, useState} from "react";
+
+// import { useRef } from "react";
+
+function App() {
+
+    // create ref
+const inputRef=useRef()
+    // create focusInput function
+function focusInput(){
+  inputRef.current.focus()
+}
+    return (
+        <div>
+
+            <input ref={inputRef}/>
+
+
+            <button>
+                Click Me
+            </button>
+      
+
+        </div>
+    );
+}
+
+export default App;
