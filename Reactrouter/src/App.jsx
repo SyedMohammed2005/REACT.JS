67b0c99380c5c8d029fee918875ba877942b1ed1
SyedMohammed2005 <syedmdpashaquadri2005@gmail.com>
2026-07-11 22:20:39 +0530

    Initial commit

---
+++ Reactrouter/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Navbar from "./Components/Navbar.jsx";
+
+function App(){
+  return(
+    <>
+      <Navbar/>
+        <Routes>
+
+<Route path="/" element={<Home/>}/>
+<Route path="/about" element={<About/>}/>
+<Route path="/contact" element={<Contact/>}/>
+    </Routes>
+    </>
+  )
+
+
+}
+export default App;
