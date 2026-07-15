1b70dc5d4d76ed0c01fe5569ce258e63def844bf
SyedMohammed2005 <syedmdpashaquadri2005@gmail.com>
2026-07-15 21:14:03 +0530

    initial commit

---
+++ axios.get/src/App.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import api from "./services/api";
+
+function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchUsers() {
+
+      try {
+
+        const response = await api.get("/users");
+
+        setUsers(response.data);
+
+      } catch (error) {
+
+        console.log(error);
+
+      }
+
+    }
+
+    fetchUsers();
+
+  }, []);
+
+  return (
+
+    <div>
+
+      <h1>Axios GET Example</h1>
+
+      {
+
+        users.map((user) => (
+
+          <div
+            key={user.id}
+            style={{
+              border: "1px solid black",
+              padding: "10px",
+              margin: "10px",
+            }}
+          >
+
+            <h3>{user.name}</h3>
+
+            <p>Email : {user.email}</p>
+
+            <p>Phone : {user.phone}</p>
+
+            <p>Company : {user.company.name}</p>
+
+          </div>
+
+        ))
+
+      }
+
+    </div>
+
+  );
+
+}
+
+export default App;
