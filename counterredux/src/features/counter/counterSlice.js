1bac9041a0ef812c3dd974b900eb1e3039d380be
SyedMohammed2005 <syedmdpashaquadri2005@gmail.com>
2026-07-19 07:44:12 +0530

    inital commit

---
+++ counterredux/src/features/counter/counterSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+
+  initialState,
+
+  reducers: {
+    increment: (state) => {
+      state.value++;
+      console.log("Incremented value:", state.value);
+    },
+
+    decrement: (state) => {
+      state.value--;
+    },
+
+    reset: (state) => {
+      state.value = 0;
+    },
+  },
+});
+
+export const { increment, decrement, reset } = counterSlice.actions;
+
+export default counterSlice.reducer;
